5cb0cf36322d07a0591fdb5d174c1208f4e40f18
DanBonix <danheavydrive@gmail.com>
2018-05-11 15:27:01 +0200

    Add files via upload

---
+++ application/views/AccountCreate.php
@@ -0,0 +1,8 @@
+<div>Nom                       : <input type="text" name="nom" value="<?php echo $this->input->post('nom'); ?>" /></div>
+<div>Prénom                    : <input type="text" name="prenom" value="<?php echo $this->input->post('prenom'); ?>" /></div>
+<div>Email                     : <input type="text" name="email" value="<?php echo $this->input->post('email'); ?>" /></div>
+<div>Mot de Passe              : <input type="text" name="password" value="<?php echo $this->input->post('password'); ?>" /></div> 
+<div>Confirmez le Mot de Passe : <input type="text" name="confirm" value="<?php echo $this->input->post('confirm'); ?>" /></div>
+<?php
+    
+
